a1938a1c825df80377e1e4b7225d968c80fb164b
Ivan <kleimenov416@gmail.com>
2021-07-05 11:28:10 -0400

    add find longest string in array challenge with forEach function

---
+++ longest_string_in_array_2.js
@@ -0,0 +1,29 @@
+let text = ['Comprehensive', 'community-driven', 'list', 'of', 'essential', 'JavaScript', 'interview', 'questions']
+let empty_arr = []
+const chack_length_of_string = (string) => {
+    let longest_string_length;
+    let longest_string;
+    if(string.length > 0) {
+        longest_string = string[0];
+        longest_string_length = string[0].length;
+        // for (let item of string) {
+        //     if(longest_string_length < item.length) {
+        //         longest_string_length = item.length;
+        //         longest_string = item;
+        //     }   
+        // }
+        // console.log('the longets string is', longest_string, 'length is equal to:', longest_string_length);
+        string.array.forEach(element => {
+            if(element.length > longest_string_length) {
+                longest_string_length = element.length; 
+                longest_string = element;
+            }
+        });
+        console.log('the longets string is', longest_string, 'length is equal to:', longest_string_length);
+    } else {
+        console.log('This is the empty array')
+    }
+}
+
+chack_length_of_string(text)
+chack_length_of_string(empty_arr)
