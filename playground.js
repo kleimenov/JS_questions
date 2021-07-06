20887eac4bd644ffee28adbbcbe0f764c68717f3
Ivan <kleimenov416@gmail.com>
2021-07-06 10:03:36 -0400

    add playground file

---
+++ playground.js
@@ -0,0 +1,27 @@
+let cars = [{
+    brand: 'ford',
+    model: 'f-150',
+    color: ['gray', 'pink', 'yellow']
+},
+{
+    brand: 'dodge',
+    model: 'Ram',
+    color: ['white', 'green', 'blue']
+}, 
+{
+    brand: 'chevy',
+    model: 'camaro',
+    color: ['white', 'green', 'blue']
+}]
+
+// for(let item in cars) {
+//     console.log(item, cars[item].brand)
+// }
+// for(let item of cars) {
+//     console.log(item)
+// }
+
+var i = 10
+var i = 'string'
+
+console.log(i)
